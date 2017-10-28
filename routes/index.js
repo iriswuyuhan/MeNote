5caf004387970e04e5e7e0915632ad55acd06beb
151250162 <151250162@smail.nju.edu.cn>
2017-10-28 21:58:14 +0800

    login demo

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* LOGIN page. */
+router.get('/login', function (req, res, next) {
+    res.render('login', {title: 'Express'});
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,35 @@
+//处理用户事务
 var express = require('express');
 var router = express.Router();
 
+var bodyparser = require('body-parser');
+var urlEncoded = bodyparser.urlencoded({extended: false});
+
+var sqlite = require('sqlite3');
+var db = new sqlite.Database('./database/test.db');
+
 //TODO 需要做一些拦截器
 
-router.get('/login', function (req, res, next) {
-    //这里应该用sqlite处理登录
-    
+router.post('/login', urlEncoded, function (req, res, next) {
+    var params = req.body;
+    var username = params.username;
+    var password = params.password;
+
+    //这里用sqlite处理登录
+    var stmnt = "select * from user where name='" + username + "'";
+    db.all(stmnt, function (err, result) {
+        if (!err) {
+            //TODO 这里需要对结果集进行一些判断
+            if (password == result[0]['password']) {
+                console.log('success');
+                //改一下
+                res.redirect('../');
+            }
+        }
+        else {
+            console.error(err);
+        }
+    });
 });
 
 module.exports = router;
