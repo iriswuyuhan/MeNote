f8f93e161a6bf4eb8b1a06734c5870edaf0583e9
151250162 <151250162@smail.nju.edu.cn>
2017-10-31 19:40:23 +0800

    note js v1.0

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,10 @@ router.get('/login', function (req, res, next) {
     res.render('login', {title: 'Express'});
 });
 
+
+/*SIGNUP page*/
+router.get('/signup', function (req, res, next) {
+    res.render('signup', {title: 'Express'});
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,12 @@ var urlEncoded = bodyparser.urlencoded({extended: false});
 var sqlite = require('sqlite3');
 var db = new sqlite.Database('./database/test.db');
 
-//TODO 需要做一些拦截器
+var fs = require('fs');
 
+var path = "./notes/";
+
+//TODO 需要做一些拦截器
+//filepath=./note
 router.post('/login', urlEncoded, function (req, res, next) {
     var params = req.body;
     var username = params.username;
@@ -21,9 +25,15 @@ router.post('/login', urlEncoded, function (req, res, next) {
         if (!err) {
             //TODO 这里需要对结果集进行一些判断
             if (password == result[0]['password']) {
-                console.log('success');
-                //改一下
-                res.redirect('../');
+                //读取用户note文件
+                fs.readdir(path + username, function (err, files) {
+                    if (!err) {
+                        res.render('note', {files: files});
+                    }
+                    else {
+                        console.error(err);
+                    }
+                });
             }
         }
         else {
